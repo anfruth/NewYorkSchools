38ac87a1d80db7579458a9f585c5e1d4d43b1b59
Andrew Fruth <anfruth@gmail.com>
2019-03-09 13:33:42 -0800

    Initial Commit

---
+++ 20190309-AndrewFruth-NYCSchoolsTests/_0190309_AndrewFruth_NYCSchoolsTests.swift
@@ -0,0 +1,34 @@
+//
+//  _0190309_AndrewFruth_NYCSchoolsTests.swift
+//  20190309-AndrewFruth-NYCSchoolsTests
+//
+//  Created by Andrew Fruth on 3/9/19.
+//  Copyright © 2019 Andrew Fruth. All rights reserved.
+//
+
+import XCTest
+@testable import _0190309_AndrewFruth_NYCSchools
+
+class _0190309_AndrewFruth_NYCSchoolsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
